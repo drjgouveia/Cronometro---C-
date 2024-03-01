e4b6f955e2a922553b2fcb7cd7e79f2c3f7ffa41
drjgouveia <drjgouveia@WIN-VM>
2024-03-01 21:30:57 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace DAFicha1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusHora = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusContador = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonAction = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.listCronometer = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusHora,
+            this.toolStripStatusContador});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(522, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusHora
+            // 
+            this.toolStripStatusHora.Name = "toolStripStatusHora";
+            this.toolStripStatusHora.Size = new System.Drawing.Size(49, 17);
+            this.toolStripStatusHora.Text = "00:00:00";
+            // 
+            // toolStripStatusContador
+            // 
+            this.toolStripStatusContador.Name = "toolStripStatusContador";
+            this.toolStripStatusContador.Size = new System.Drawing.Size(49, 17);
+            this.toolStripStatusContador.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonAction
+            // 
+            this.buttonAction.Location = new System.Drawing.Point(141, 402);
+            this.buttonAction.Name = "buttonAction";
+            this.buttonAction.Size = new System.Drawing.Size(75, 23);
+            this.buttonAction.TabIndex = 1;
+            this.buttonAction.Text = "Run";
+            this.buttonAction.UseVisualStyleBackColor = true;
+            this.buttonAction.Click += new System.EventHandler(this.buttonAction_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(222, 402);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // listCronometer
+            // 
+            this.listCronometer.FormattingEnabled = true;
+            this.listCronometer.Location = new System.Drawing.Point(12, 12);
+            this.listCronometer.Name = "listCronometer";
+            this.listCronometer.Size = new System.Drawing.Size(498, 381);
+            this.listCronometer.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listCronometer);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonAction);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "A minha aplicação";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusHora;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusContador;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonAction;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.ListBox listCronometer;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAFicha1
+{
+    public partial class Form1 : Form
+    {
+        private DateTime contadorTempo;
+        private bool isContadorRunning;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.contadorTempo = new DateTime(0);
+            this.isContadorRunning = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusHora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void buttonAction_Click(object sender, EventArgs e)
+        {
+            if (this.isContadorRunning)
+            {
+                this.isContadorRunning = false;
+                buttonAction.Text = "Run";
+            } 
+            else
+            {
+                this.isContadorRunning = true;
+                buttonAction.Text = "Stop";
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            this.contadorTempo = new DateTime(0);
+            this.isContadorRunning = false;
+            buttonAction.Text = "Run";
+            toolStripStatusContador.Text = "00:00:00.000";
+            listCronometer.Items.Clear();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (isContadorRunning)
+            {
+                contadorTempo = contadorTempo.AddMilliseconds(10);
+                toolStripStatusContador.Text = contadorTempo.ToString("HH:mm:ss.fff");
+            }
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            listCronometer.Items.Add(DateTime.Now.ToLongTimeString() + " @ " + this.contadorTempo.ToString("HH:mm:ss.fff"));
+        }
+    }
+}
